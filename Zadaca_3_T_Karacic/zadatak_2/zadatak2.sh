b5a28d4af84c4307a901373c369338aab19e4c98
tkaracic-gif <tkaracic@student.unipu.hr>
2026-04-27 09:10:51 +0200

    .

--- Zadaca_3_T_Karacic/zadatak_2/zadatak2.sh
+++ Zadaca_3_T_Karacic/zadatak_2/zadatak2.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+
 
 BROJ=$1
 
-# Provjera je li broj između 1 i 10
+
 if [[ "$BROJ" -lt 1 || "$BROJ" -gt 10 ]]; then
     echo "Error: Broj mora biti od 1 do 10."
     exit 1
 fi
 
-# Generiraj brojeve i spremi u datoteku (overwrite način)
+
 seq 1 "$BROJ" > brojevi.txt
 echo "Gotovo! Pogledaj brojevi.txt"
